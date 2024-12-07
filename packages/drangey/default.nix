a6762d80923689f34841358d423bde109d717a13
Jón Bjarni Bjarnason <jon@centroid.is>
2024-12-07 18:03:50 +0000

    drangey now is downloadable and installable

--- packages/drangey/default.nix
+++ packages/drangey/default.nix
@@ -4,17 +4,25 @@ let
   version = "2024.12.1";
   src = let
     token = builtins.getEnv "GITHUB_TOKEN";
-  in runCommand "drangey-${version}.tar.gz" {
-    nativeBuildInputs = [ bash curl wget jq ];
-    inherit token;
-  } ''
-    bash ${downloadScript} \
-      -t $token \
-      -r centroid-is/linescan \
-      -v v${version} \
-      -f linescan-${version}-Linux-x86_64-Drangey.tar.gz \
-      -o $out
-  '';
+    filename = "linescan-${version}-Linux-x86_64-Drangey.tar.gz";
+    
+    # First, fetch the release information
+    releaseInfo = builtins.fromJSON (builtins.readFile (fetchurl {
+      url = "https://api.github.com/repos/centroid-is/linescan/releases";
+      curlOptsList = [ "-H" "Authorization: Bearer ${token}" "-H" "Accept: application/vnd.github.v3.raw" ];
+      sha256 = "sha256-gdtycuTprwGLW7eRgmp3HkboXymlvHOj0r934qmoFFM=";
+    }));
+    
+    # Find the correct release and asset
+    release = builtins.head (builtins.filter (r: r.tag_name == "v${version}") releaseInfo);
+    asset = builtins.head (builtins.filter (a: a.name == filename) release.assets);
+    
+  in fetchurl {
+    name = "drangey.tar.gz";
+    url = "https://api.github.com/repos/centroid-is/linescan/releases/assets/${toString asset.id}";
+    curlOptsList = [ "-H" "Accept: application/octet-stream" "-H" "Authorization: Bearer ${token}" ];
+    sha256 = "sha256-kgu1rXxwKksyihAtk0YooGmfZtyu5irlweRQuF3wAQ4=";
+  };
   package = stdenv.mkDerivation {
     pname = "drangey";
     inherit version src;
@@ -29,6 +37,7 @@ let
       (import ../pylon {     # Import pylon from the same flake
         inherit pkgs lib stdenv fetchurl;
       }).package             # Get the package attribute
+      pkgs.python3Packages.pytorch  # Add PyTorch for libtorch_cpu.so and libc10.so
     ];
 
     installPhase = ''
